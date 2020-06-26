703e36f5a58a2e6ba630ad958e8712eaddeeda62
Guido Zamudio <ggerman048@gmail.com>
2020-06-26 03:03:22 -0300

    greater

---
+++ Php/Ejercicio 10/salidaVisitas.php
@@ -0,0 +1,46 @@
+<?php
+sleep(3);
+
+$conexion = new mysqli("localhost","labo3","abc123","labo3");
+
+if($conexion->connect_errno){
+	echo "Falló la conexion " . $conexion->connect_errno;
+}
+
+$conexion->set_charset("utf8");
+
+$sql = "SELECT * FROM gimnasio";
+
+$resultado=$conexion->query($sql);
+
+if($conexion->errno){
+	die($conexion->error);
+}
+		
+
+$resultadoCuentaRegistros = $resultado->num_rows;
+
+$visitas=[];
+
+while($fila=$resultado->fetch_assoc()){
+    $objVisit=new stdClass();
+    $objVisit->ID=$fila['ID'];
+    $objVisit->Nombre=$fila['Nombre'];
+    $objVisit->Apellido=$fila['Apellido'];
+    $objVisit->Actividad=$fila['Actividad'];
+    $objVisit->Fecha=$fila['Fecha'];
+    array_push($visitas,$objVisit);
+}
+
+$objVisitas=new stdClass();
+
+$objVisitas->visitas=$visitas;
+
+$objVisitas->cuenta=$resultadoCuentaRegistros;
+
+$salidaJSON=json_encode($objVisitas);
+
+$conexion->close();
+
+echo $salidaJSON;
+?>
